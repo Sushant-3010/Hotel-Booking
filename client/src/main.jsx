d84b63cfb41299dbfc29b48bdb15eabcdf606d4b
Sushant-3010 <sushantchothe405@gmail.com>
2025-07-30 16:42:32 +0530

    project completed

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,9 +4,12 @@ import './index.css'
 import App from './App.jsx'
 import{ BrowserRouter } from 'react-router-dom'
 import { ClerkProvider } from '@clerk/clerk-react'
+import { AppProvider } from './context/AppContext.jsx'
 
 // Import your Publishable Key
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
+
+
 
 if (!PUBLISHABLE_KEY) {
   throw new Error('Add your Clerk Publishable Key to the .env file')
@@ -15,7 +18,10 @@ if (!PUBLISHABLE_KEY) {
 createRoot(document.getElementById('root')).render(
   <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
   <BrowserRouter>
+  <AppProvider>
     <App />
+  </AppProvider>
+    
   </BrowserRouter>
   </ClerkProvider>,
 )
